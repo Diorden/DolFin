eed8b37d739ca6cfefd8982e0c2c7a746089d156
Diorden <cameroncross007@gmail.com>
2024-09-04 20:54:47 +0100

    2-place decimal number inputs now supported

--- js/dolfin.js
+++ js/dolfin.js
@@ -2,14 +2,16 @@
 
 
 function addAccount() {
-  let accName = document.querySelector("#accName").value;
-  let accBal = parseInt(document.querySelector("#accBal").value);
-  let accPercent = document.querySelector("#accPercent").value;
 
+  let balanceStr = document.querySelector("#accBal").value;
+  let balanceInt = parseInt(balanceStr.replace('.', ''));
+  
+  balanceInt *= checkDecimalSeperator(balanceStr) ? 1 : 100;
+  
   let account = {
-    "name": accName,
-    "balance": accBal,
-    "percentFromIncome": accPercent / 100,
+    "name": document.querySelector("#accName").value,
+    "balance": balanceInt,
+    "percentFromIncome": document.querySelector("#accPercent").value / 100,
     "ID": accounts.length
   };
 
@@ -24,22 +26,20 @@ function removeAccount(ID) {
 }
 
 function addTransaction() {
-  let transName = document.querySelector("#transName").value;
-  let transCost = parseInt(document.querySelector("#transCost").value);
-  let transDate = document.querySelector("#transDate").value;
-  let itemSubPeriod = document.querySelector("#itemSubTime").value;
-  let renewalTime = parseInt(document.querySelector("#renewalTime").value);
-  let accountName = (document.querySelector("#transAccName").value).toLowerCase();
-  let transCategory = document.querySelector("#transCategory").value;
+  
+  let costStr = document.querySelector("#transCost").value;
+  let costInt = parseInt(costStr.replace('.', ''));
 
+  costInt *= checkDecimalSeperator(costStr) ? 1 : 100;
+  
   let transaction = {
-    "name": transName,
-    "cost": transCost,
-    "date": transDate,
-    "accountName": accountName,
-    "itemSubPeriod": itemSubPeriod,
-    "renewalTime": renewalTime,
-    "category": transCategory,
+    "name": document.querySelector("#transName").value,
+    "cost": costInt,
+    "date": document.querySelector("#transDate").value,
+    "accountName": (document.querySelector("#transAccName").value).toLowerCase(),
+    "itemSubPeriod": document.querySelector("#itemSubTime").value,
+    "renewalTime": parseInt(document.querySelector("#renewalTime").value),
+    "category": document.querySelector("#transCategory").value,
     "ID": transactions.length
   };
 
@@ -66,14 +66,17 @@ function applyTransaction(transaction) {
 }
 
 function applyIncome() {
-  let income = parseInt(document.querySelector("#income").value);
+  let incomeStr = document.querySelector("#income").value;
   let accountName = (document.querySelector("#incomeAccName").value).toLowerCase();
   let accountFound = false;
+  let incomeInt = parseInt(incomeStr.replace('.', ''));
+  
+  incomeInt *= checkDecimalSeperator(incomeStr) ? 1 : 100;
   
   accounts.forEach((account) => {
     if (accountName === account.name.toLowerCase() || accountName === "all" || accountName === "") {
       accountFound = true;
-      account.balance += income * account.percentFromIncome;
+      account.balance += incomeInt * account.percentFromIncome;
     }
   })
   
@@ -82,6 +85,16 @@ function applyIncome() {
     
   update();
 }
+
+function checkDecimalSeperator(numberStr) {
+  for (let i=0; i<numberStr.length; i++) {
+    if (numberStr[i] === '.')
+      return true;
+  }
+  return false;
+}
+
+/*function countDecimalPlaces(*/
 
 
 function updateDownloadHREF() {
@@ -92,19 +105,28 @@ function updateDownloadHREF() {
   accountsDL.href = url;
   accountsDL.download = "accounts.json";
 }
+
+function displayAsFloat(number, decimalPlaces) {
+  let numberStr = number.toString();
+  let sigFigs = numberStr.slice(0, numberStr.length - decimalPlaces);
+  let decimalFigs = numberStr.slice(numberStr.length - decimalPlaces, numberStr.length);
   
+  
+  return `${sigFigs}.${decimalFigs}`;
+}
+
 function updateOverview() {
   let overview = `<table><tr><th colspan="3">Accounts</th</tr><tr><th>Account Name</th><th>Account Balance</th><th>Delete</th></tr>`;
   let transactionView = `<table><tr><th colspan="6">Transactions</th></tr><tr><th>Name</th><th>Cost</th><th>Date</th><th>Category</th><th>Account</th><th>Delete</th></tr>`;
   let subscriptionView = `<table><tr><th colspan="5">Subscriptions</th></tr><tr><th>Name</th><th>Cost</th><th>Renews</th><th>Category</th><th>Delete</th></tr>`;
   
   accounts.forEach((account) => {
-    overview += `<tr><td>${account.name}</td><td>${account.balance}</td><td><button class="deleteButton" onclick="removeAccount(${account.ID})">X</button></td></tr>`; 
+    overview += `<tr><td>${account.name}</td><td>${displayAsFloat(account.balance, 2)}</td><td><button class="deleteButton" onclick="removeAccount(${account.ID})">X</button></td></tr>`; 
   })
   
   for (let ID = transactions.length - 1; ID > transactions.length - 6 && ID>=0; ID--) {
     const { name, cost, date, accountName, itemSubPeriod, renewalTime, category } = transactions[ID]
-    transactionView += `<tr><td>${name}</td><td>${cost}</td><td>${date}</td><td>${category}</td><td>${accountName}</td><td><button class="deleteButton" onclick="removeTransaction(${ID})">X</button></td></tr>`;
+    transactionView += `<tr><td>${name}</td><td>${displayAsFloat(cost, 2)}</td><td>${date}</td><td>${category}</td><td>${accountName}</td><td><button class="deleteButton" onclick="removeTransaction(${ID})">X</button></td></tr>`;
   }
   
   overview += `</table>`;
@@ -125,7 +147,7 @@ function update() {
 }
 
 
-
+let UKPound = new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' });
 let accounts = JSON.parse(localStorage.getItem("accountList")) || [];
 let transactions = JSON.parse(localStorage.getItem("transactionList")) || [];
 let fileHandle;
